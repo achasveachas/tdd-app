a1eb4c4de0fa28582be1b257187621e6df0a1aa0
Yechiel K <chilikkal@gmail.com>
2017-06-21 23:53:34 -0400

    added tests for which tasks are part of velocity

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,3 +9,24 @@ RSpec.describe Project do
 
   end
 end
+
+describe "velocity" do
+  let(:task) { Task.new(size: 3) }
+
+  it "does not count an incomplete task towards velocity" do
+    expect(task).not_to be_part_of_velocity
+    expect(task.point_toward_velocity).to eq(0)
+  end
+
+  it "does not count a long-ago task towards velocity" do
+    task.mark_completed(6.months.ago)
+    expect(task).not_to be_part_of_velocity
+    expect(task.point_toward_velocity).to eq(0)
+  end
+
+  it "does not count an incomplete task towards velocity" do
+    task.mark_completed(1.day.ago)
+    expect(task).to be_part_of_velocity
+    expect(task.point_toward_velocity).to eq(3)
+  end
+end
