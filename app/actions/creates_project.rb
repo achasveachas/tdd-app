d019d2fa05805992760d4a6598fde40d08d70658
Yechiel K <chilikkal@gmail.com>
2017-07-05 22:49:06 -0400

    added functionalite to pars a string with one task

--- app/actions/creates_project.rb
+++ app/actions/creates_project.rb
@@ -1,17 +1,19 @@
 class CreatesProject
   attr_accessor :name, :task_string, :project
 
-  def initialize(name = "", task_string = "")
+  def initialize(name: "", task_string: "")
     @name = name
     @task_string = task_string
   end
 
   def build
-    self.project = Project.new(name)
+    self.project = Project.new(name: name)
   end
 
   def convert_string_to_tasks
     tasks = []
+    tasks << Task.new(title: self.task_string, size: 1) if self.task_string != ""
+    tasks
   end
 
 end
